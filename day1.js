59faaa00a750dc79092b1a4e6b51e08531701db9
TheshaAravinthan06 <aravinthanthesha@gmail.com>
2025-11-10 11:37:45 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,34 @@
+
+
+// Hoisting
+console.log(num);
+let num= 25;
+
+console.log(num);
+var num= 25;
+
+console.log(num);
+const num= 25;
+
+
+// Scope
+
+// Function scope
+{
+    let x =25
+    console.log(x)
+}
+console.log(x)
+
+// Block scope
+{
+    const x =25
+    console.log(x)
+}
+console.log(x)
+
+{
+    let x =25
+    console.log(x)
+}
+console.log(x)
